c630d0214b17f0587ea24559dfd8edb30edde332
정선정_데이터서비스개발팀 <sunjeong0420@sk.com>
2026-08-03 14:32:04 +0900

    update: login api url 수정

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -1,17 +1,17 @@
-import { api } from "./axios";
-import { LoginRequest, LoginResponse, SignupRequest, User } from "@/types/auth";
+import { api } from './axios';
+import { LoginRequest, LoginResponse, SignupRequest, User } from '@/types/auth';
 
 export const loginApi = async (data: LoginRequest) => {
-  const response = await api.post<LoginResponse>("/auth/login", data);
+  const response = await api.post<LoginResponse>('/login', data);
   return response.data;
 };
 
 export const signupApi = async (data: SignupRequest) => {
-  const response = await api.post<User>("/auth/signup", data);
+  const response = await api.post<User>('/auth/signup', data);
   return response.data;
 };
 
 export const meApi = async () => {
-  const response = await api.get<User>("/users/me");
+  const response = await api.get<User>('/users/me');
   return response.data;
 };
