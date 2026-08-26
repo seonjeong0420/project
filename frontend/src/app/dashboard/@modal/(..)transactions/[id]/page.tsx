a0b0b1a4698315c0579a4a999bf01acb725574f6
정선정 <seonjeong0420@sk.com>
2026-08-26 12:52:25 +0900

    fix: intercepting route transactions 페이지 수정

--- frontend/src/app/dashboard/@modal/(..)transactions/[id]/page.tsx
+++ frontend/src/app/dashboard/@modal/(..)transactions/[id]/page.tsx
@@ -1,16 +1,27 @@
+'use client';
+import { use } from 'react';
 import Modal from '@/components/ui/Modal';
+import { useTransactionDetail } from '@/hooks/useTransaction';
 
 type Props = {
   params: Promise<{ id: string }>;
 };
 
-const TransactionsModal = async ({ params }: Props) => {
-  const { id } = await params;
+const TransactionsModal = ({ params }: Props) => {
+  const { id } = use(params);
+  const { data } = useTransactionDetail(id);
 
   return (
     <Modal>
       <h1>내역관리</h1>
       <span>transaction ID : {id}</span>
+      <ul>
+        <li>타입 : {data?.type}</li>
+        <li>타이틀 : {data?.title}</li>
+        <li>금액 : {data?.amount}</li>
+        <li>메모 : {data?.memo}</li>
+        <li>날짜 : {data?.date}</li>
+      </ul>
     </Modal>
   );
 };
